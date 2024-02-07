d66658b260b7d195820762571aff23cb077eba15
nalbion <nalbion@yahoo.com>
2024-02-07 15:17:50 +1100

    WebCommandProcessor

---
+++ src/commands/web-command-processor.ts
@@ -0,0 +1,60 @@
+import CommandProcessor from "./command-processor";
+import { RobotController } from "../robot";
+
+export default class WebCommandProcessor extends CommandProcessor {
+  override readCommands(controller: RobotController): void {
+    for (const id of ["place", "move", "left", "right", "report"]) {
+      // enable the control
+      document.getElementById(id)!.removeAttribute("disabled");
+
+      // register the event listener, using processCommand()
+      let command = id.toUpperCase();
+
+      document.getElementById(id)!.addEventListener("click", (e: MouseEvent) => {
+        let command = (e.target as HTMLButtonElement).id.toUpperCase();
+        command = this.addCommandArgs(command);
+
+        this.processCommand(command, controller);
+      });
+    }
+
+    const keyMapping = {
+      ArrowUp: "MOVE",
+      ArrowLeft: "LEFT",
+      ArrowRight: "RIGHT",
+      ArrowDown: "REPORT",
+      " ": "PLACE",
+      Escape: "EXIT",
+    };
+
+    window.addEventListener("keydown", (event) => {
+      if (event.key in keyMapping) {
+        const key = event.key as keyof typeof keyMapping;
+        let command = keyMapping[key];
+        command = this.addCommandArgs(command);
+        this.processCommand(command, controller);
+      }
+    });
+  }
+
+  protected override provideReport(controller: RobotController): void {
+    const output = controller.report();
+    document.getElementById("output")!.textContent = output;
+  }
+
+  protected override exit(): void {
+    if (confirm("Are you sure you want to exit?")) {
+      window.close();
+    }
+  }
+
+  private addCommandArgs(command: string) {
+    if (command === "PLACE") {
+      const x = (document.getElementById("x") as HTMLInputElement).value;
+      const y = (document.getElementById("y") as HTMLInputElement).value;
+      const facing = (document.getElementById("facing") as HTMLInputElement).value;
+      command += ` ${x},${y},${facing}`;
+    }
+    return command;
+  }
+}
--- src/ui.ts
+++ src/ui.ts
@@ -1,5 +1,6 @@
-import { RobotController, FacingDirection } from "./robot";
+import { RobotController } from "./robot";
 import VisualSimulation from "./robot/visual-simulation";
+import WebCommandProcessor from "./commands/web-command-processor";
 
 const robot = new VisualSimulation();
 const width = 5;
@@ -8,60 +9,8 @@ const depth = 5;
 document.getElementById("x")!.setAttribute("max", width.toString());
 document.getElementById("y")!.setAttribute("max", depth.toString());
 
-robot
-  .init(document.getElementById("robot") as HTMLDivElement, width, depth)
-  .then(() => {
-    // Once initialised, enable the controls
-    for (const id of ["place", "move", "left", "right", "report"]) {
-      document.getElementById(id)!.removeAttribute("disabled");
-    }
-  });
-
-const controller = new RobotController(width, depth, robot);
-
-function report() {
-  const output = controller.report();
-  document.getElementById("output")!.textContent = output;
-}
-
-window.addEventListener("keydown", (event) => {
-  switch (event.key) {
-    case "ArrowUp":
-      controller.move();
-      break;
-    case "ArrowLeft":
-      controller.rotateLeft();
-      break;
-    case "ArrowRight":
-      controller.rotateRight();
-      break;
-    case "ArrowDown":
-      report();
-  }
+robot.init(document.getElementById("robot") as HTMLDivElement, width, depth).then(() => {
+  const controller = new RobotController(width, depth, robot);
+  const processor = new WebCommandProcessor();
+  processor.readCommands(controller);
 });
-
-function addClickListener(id: string, handler: () => void) {
-  document.getElementById(id)!.addEventListener("click", handler);
-}
-
-addClickListener("place", () => {
-  const x = Number((document.getElementById("x") as HTMLInputElement).value);
-  const y = Number((document.getElementById("y") as HTMLInputElement).value);
-  const facing = (document.getElementById("facing") as HTMLInputElement).value as FacingDirection;
-
-  controller.placeRobot(x, y, facing);
-});
-
-addClickListener("move", () => {
-  controller.move();
-});
-
-addClickListener("left", () => {
-  controller.rotateLeft();
-});
-
-addClickListener("right", () => {
-  controller.rotateRight();
-});
-
-addClickListener("report", report);
